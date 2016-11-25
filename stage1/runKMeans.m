acd7a6223f2e0dd127efe55857c186c212bc7b1b
LAPTOP-HKOIM4F6\David <solomond78@gmail.com>
2016-11-25 14:04:14 +0200

    added siftFlow and opticalFlow libraries

--- stage1/runKMeans.m
+++ stage1/runKMeans.m
@@ -1,8 +1,27 @@
 
-dataFolder = 'C:\Users\David\Desktop\matlab\matconvnet\matconvnet-1.0-beta23\matconvnet-1.0-beta23\data\fish\train\train';
-dataFolder = [dataFolder, '\NoF'];
+% initialize paths
+vl_rootnnPath = 'C:\Users\David\Desktop\matlab\matconvnet\matconvnet-1.0-beta23\matconvnet-1.0-beta23';
+fishType = 'ALB';
+addpath(genpath(vl_rootnnPath))
+try
+    warning off;
+    vl_setupnn();   
+catch
+    disp('you need to initialize the matconvnet path first');
+end
+dataFolder = fullfile(vl_rootnnPath, 'data\fish\train\train');
+dataFolder = [dataFolder, '\', fishType];
+if ~exist(dataFolder, 'dir')
+    disp('fish data path should look like : ');
+    disp([blanks(3),'...vl_rootnn\data\fish\train\train']);
+end
 
+% include folders and subfolders of this project
+addpath(genpath(fileparts(fileparts(mfilename('fullpath')))))
+
+%lets start
 imageNames = dir([dataFolder,  '\*.jpg']);
+%height and width of the resized images
 h = 400;
 images.data = zeros(numel(imageNames), h*h);
 images.path = cell(numel(imageNames),1);
@@ -11,6 +30,7 @@ for imageIdx = 1:numel(imageNames)
     img = imread(fullfile(dataFolder, imageNames(imageIdx).name));
     img = rgb2gray(img);
     img = imresize(img, [h,h]);
+    % proved to work better without these lines
 %     img = img - mean(img(:));
 %     img = img / std(img(:));
     images.data(imageIdx, :) = reshape(img, [h*h, 1]);
@@ -21,7 +41,7 @@ save images images '-v7.3'
 % load ('images.mat');
 
 % calculate KMeans
-k = 15;
+k = 15; %you can play with that...
 [label, centroid, dis] = fkmeans(images.data, k);
 centroid = centroid';
 
@@ -33,6 +53,8 @@ for classIdx = 1:k
     title(['class number : ',num2str(classIdx)]);
 end
 
+% rewrite images to the cluster dirs
+% BECAREFULL NOT TO OVERWRITE IN CASE YOU HAVE RUN THIS CODE ONCE BEFORE
 for classIdx = 1:k
     disp(['class number : ',num2str(classIdx), '/', num2str(k)]);
     path = [dataFolder, '\', num2str(classIdx)];
@@ -50,7 +72,7 @@ for classIdx = 1:k
 end
 
 
-
+% display all images from a desiredClass
 % desiredClass = 4;
 % for imageIdx = 1:size(images.data, 1)
 %     if label(imageIdx) == desiredClass
